0da4250855b7bb407ead29886c0ced212e103478
robotcdf <cdaofang@gmail.com>
2017-05-16 22:27:11 +0800

    download question

---
+++ Java/81.search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,31 @@
+/*
+ * [81] Search in Rotated Sorted Array II
+ *
+ * https://leetcode.com/problems/search-in-rotated-sorted-array-ii
+ *
+ * Medium (32.84%)
+ * Total Accepted:    90854
+ * Total Submissions: 276605
+ * Testcase Example:  '[]\n5'
+ *
+ * 
+ * Follow up for "Search in Rotated Sorted Array":
+ * What if duplicates are allowed?
+ * 
+ * Would this affect the run-time complexity? How and why?
+ * 
+ * 
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown
+ * to you beforehand.
+ * 
+ * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+ * 
+ * Write a function to determine if a given target is in the array.
+ * 
+ * The array may contain duplicates.
+ */
+public class Solution {
+    public boolean search(int[] nums, int target) {
+        
+    }
+}
