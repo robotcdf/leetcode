85e19d33fb6355bc1a256a8f81f3ceabb1829af3
robotcdf <cdaofang@gmail.com>
2017-05-18 09:10:52 +0800

    81. Search in Rotated Sorted Array II

--- Java/81.search-in-rotated-sorted-array-ii.java
+++ Java/81.search-in-rotated-sorted-array-ii.java
@@ -26,6 +26,31 @@
  */
 public class Solution {
     public boolean search(int[] nums, int target) {
+	int first=0, last=nums.length;
+        int mid = 0;
+        
+        while(first != last){
+            mid = first + (last-first)/2;
+            if(nums[mid] == target)
+                return true;
+                
+            if(nums[first] < nums[mid]){
+                if(nums[first] <= target && target < nums[mid]){
+                    last = mid;
+                }else{
+                    first = mid+1;
+                }
+            }else if(nums[first] > nums[mid]){
+                if(nums[mid] < target && target <= nums[last-1]){
+                    first = mid+1;
+                }else{
+                    last = mid;
+                }
+            }else{
+                first++;
+            }
+        }
+        return false;
         
     }
 }
