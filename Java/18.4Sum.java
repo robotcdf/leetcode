4b030ddb35d0863c814ec103764ca11fea7a9d58
robotcdf <cdaofang@gmail.com>
2017-06-26 22:54:51 +0800

    18. 4Sum

---
+++ Java/18.4Sum.java
@@ -0,0 +1,53 @@
+/*
+ * [18] 4Sum  
+ *
+ * https://leetcode.com/problems/4sum/
+ *
+ * Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target? 
+ * Find all unique quadruplets in the array which gives the sum of target.
+ * Note: The solution set must not contain duplicate quadruplets.
+ * 
+ * For example, given array S = [1, 0, -1, 0, -2, 2], and target = 0.
+ * A solution set is:
+ * [
+ *   [-1,  0, 0, 1],
+ *   [-2, -1, 1, 2],
+ *   [-2,  0, 0, 2]
+ * ]
+ */
+
+public class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Set<List<Integer>> resultSet = new HashSet<>();
+        Arrays.sort(nums);
+        
+        for(int i=0; i<nums.length-3; i++){
+            for(int j=i+1; j<nums.length-2; j++){
+                int left = j+1, right = nums.length - 1;
+                
+                while(left < right){
+                    int sum = nums[i] + nums[j] + nums[left] +nums[right];
+                    if(sum == target){
+                        //ArrayList<Integer> temp = new ArrayList<Integer>();
+                        //temp.add(nums[i]);
+                        //temp.add(nums[j]);
+                        //temp.add(nums[left]);
+                        //temp.add(nums[right]);
+                        resultSet.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                    
+                        //resultSet.add(temp);
+                        ++left;--right;
+                    }else if(sum > target){
+                        --right;
+                    }else {
+                        ++left;
+                    }
+                }
+                
+            }
+            
+        }
+        List<List<Integer>> result = new ArrayList<List<Integer>>(resultSet);
+        return result;
+    }
+}
